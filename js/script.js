7eadf768990db61bfca5f275f437907ae1c34a33
Tashby <tashby2714@gmail.com>
2016-05-27 08:16:05 -0400

    fixed isAdecimal

--- js/script.js
+++ js/script.js
@@ -1,47 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-var EnteredNum = prompt("Enter a number");
-console.log(EnteredNum);
-if (isAnumber(EnteredNum) && isAdecimal(EnteredNum)){
-FizzBuzz(EnteredNum)
-}
-else {console.log("Bad Bad User");
-window.location.reload();
-}
+    var enteredNum = prompt("Enter a number");
+    console.log(enteredNum);
+    if (isAnumber(enteredNum) && isAdecimal(enteredNum)) {
+        fizzBuzz(enteredNum)
+    } else {
+        console.log("Bad Bad User");
+        window.location.reload();
+    }
 });
 
-function isAnumber(input){
+function isAnumber(input) {
     var parsed = parseInt(input);
-    if (isNaN(parsed)){
+    if (isNaN(parsed)) {
+        console.log(parsed);
         return false;
+    } else {
+        console.log(parsed);
+        return true;
     }
-    else { return true;}
 }
 
-function isAdecimal(input){
-    var 
+function isAdecimal(input) {
+    if (input % 1 != 0){
+        Math.floor(input);
+        return false; 
+    } else {
+        return true;
+    }
 }
 
-function fizzyBuzzy (num){
+function fizzyBuzzy(num) {
 
 
-    if (num % 15 === 0){
+    if (num % 5 === 0 && num % 3 === 0) {
         return "FizzBuzz";
-    }
-    else if (num % 3 ===0){
+    } else if (num % 3 === 0) {
         return "Fizz";
-    }
-    else if (num % 5 === 0){
+    } else if (num % 5 === 0) {
         return "Buzz";
-    }
-    else{
+    } else {
         return num;
     }
 };
-    function FizzBuzz(num){
 
-        for (var i = 1; i <= num; i++) {
-            console.log(fizzyBuzzy(i))
-        } 
+function fizzBuzz(num) {
 
+    for (var i = 1; i <= num; i++) {
+        console.log(fizzyBuzzy(i))
     }
+
+}
